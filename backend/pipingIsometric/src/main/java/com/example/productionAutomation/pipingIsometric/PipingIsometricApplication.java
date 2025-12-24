bce665a515dc683ff8feb882548b138d5e9ed976
Aswath jerma <aswathjerma@Aswaths-MacBook-Pro.local>
2025-12-24 13:13:01 +0530

    Initial backend setup for piping & welding production automation MVP

---
+++ backend/pipingIsometric/src/main/java/com/example/productionAutomation/pipingIsometric/PipingIsometricApplication.java
@@ -0,0 +1,13 @@
+package com.example.productionAutomation.pipingIsometric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipingIsometricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipingIsometricApplication.class, args);
+	}
+
+}
---
+++ backend/pipingIsometric/src/test/java/com/example/productionAutomation/pipingIsometric/PipingIsometricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productionAutomation.pipingIsometric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipingIsometricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
